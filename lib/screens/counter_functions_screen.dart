95920982849e6f8a71bed7b272b5087146423881
alvarado0925 <alvarado0925@gmail.com>
2024-11-07 23:31:03 -0500

    Añade botones para incrementar y decrementar el contador, y un botón de reinicio en la pantalla de contador

--- lib/screens/counter_functions_screen.dart
+++ lib/screens/counter_functions_screen.dart
@@ -13,39 +13,77 @@ class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Counter functions screen',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '$_clickCounter',
-              style:
-                  const TextStyle(fontSize: 150, fontWeight: FontWeight.w100),
+        appBar: AppBar(
+          title: const Text(
+            'Counter functions screen',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.refresh,
+              color: Colors.white,
             ),
-            Text(
-              'Click${_clickCounter == 1 ? '' : 's'}',
-              style: const TextStyle(
-                fontSize: 25,
+            onPressed: () {
+              setState(() {
+                _clickCounter = 0;
+              });
+            },
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.exposure_plus_2,
+                color: Colors.white,
               ),
-            )
+              onPressed: () {
+                setState(() {
+                  _clickCounter += 2;
+                });
+              },
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _clickCounter++;
-          });
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '$_clickCounter',
+                style:
+                    const TextStyle(fontSize: 150, fontWeight: FontWeight.w100),
+              ),
+              Text(
+                'Click${_clickCounter == 1 ? '' : 's'}',
+                style: const TextStyle(
+                  fontSize: 25,
+                ),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  _clickCounter++;
+                });
+              },
+              child: const Icon(Icons.add),
+            ),
+            const SizedBox(height: 10),
+            FloatingActionButton(
+              shape: const StadiumBorder(),
+              onPressed: () {
+                setState(() {
+                  _clickCounter--;
+                });
+              },
+              child: const Icon(Icons.remove),
+            )
+          ],
+        ));
   }
 }
